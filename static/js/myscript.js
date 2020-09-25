37d01623cf59d8c0c7ff503adcd025fac1e9f321
chamick7 <sw.chal7.dev@gmail.com>
2020-09-25 16:52:11 +0700

    Nav: Add hamburger menu

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,10 +1,31 @@
 
 
 
+var menu_list = document.querySelectorAll('.nav_menu a');
+
+menu_list.forEach(list => {
+    list.addEventListener('click',closenav);
+    
+})
+
+
 
 function education_toggle(edu_id){
     console.log(edu_id+'_info');
 
     var element = document.querySelector('.'+edu_id+'_info');
     element.classList.toggle('show');
+}
+
+
+function shownav(){
+    var menu = document.querySelector('.nav_menu');
+
+    menu.classList.toggle('show');
+}
+
+function closenav(){
+    var menu = document.querySelector('.nav_menu');
+
+    menu.classList.remove('show');
 }
